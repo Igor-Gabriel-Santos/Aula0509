fc35acb18e0c707e26042a84953c396d2dc3d327
Igor-Gabriel-Santos <Gabrieligor022@gmail.com>
2023-09-05 17:53:21 +0000

    save02

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -1,16 +1,28 @@
 import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import './styles.css';
+import api from "./services/api"
 
 
 function App() {
 
   const [input, setInput] = useState('')
+  const [cep, setCep] = useState('')
 
 
     
-  function aoClicar() {
-    alert('O que tem dentro do input: ' + input)
+   async function aoClicar() {
+    if(input === ''){
+    alert("Amigão ? preencha com um CEP ai")
+    return
+  } try {
+    const response = await api.get(`${input}/json`)
+    setCep(response.data)
+    setInput("")
+  } catch {
+    alert('Amigão ? parece que não sabe cep ??? ')
+    setInput("")
+  }
   }
 
   return (
@@ -23,13 +35,14 @@ function App() {
           <FiSearch size={25} color="#FFF"/>
         </button>
       </div>
-
+    {Object.keys(cep).length > 0 && (
     <main className="main">
-      <h2> Cep :  19970000 </h2>
-      <span> Rua : Kathariene </span> 
-      <span> Bairro : Kathariene </span> 
-      <span> Estado : Kathariene </span> 
-    </main>
+      <h2> Cep :  {cep.cep} </h2>
+      <span> Rua : {cep.logradouro} </span> 
+      <span> Bairro : {cep.bairro} </span> 
+      <span> Estado : {cep.uf} </span> 
+    </main>)}
+    
 
 
 
